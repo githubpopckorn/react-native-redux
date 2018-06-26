186eacc06ab31bdd178179fa8425e28eb36a5f90
Santy López <santy_lopez@live.com.ar>
2018-06-25 19:56:08 -0500

    Arquitectura incial

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import createStore from './store/createStore';
+import AppContainer from './AppContainer'
+
+
+export default class Root extends React.Component {
+
+  renderApp(){
+    const initialState = window.__INITIAL_STATE___;
+    const store = createStore(initialState);
+
+    return(
+      <AppContainer store={store}/>
+    );
+  }
+
+  render(){
+    return this.renderApp();
+  }
+}
